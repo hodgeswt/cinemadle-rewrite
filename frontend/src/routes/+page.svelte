b1fee37e9719770794780d5c5060b7f4be87182c
Will Hodges <hodges.wt@gmail.com>
2025-03-16 20:33:35 +0000

    custom search suggestiosn

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,7 +3,6 @@
     import { Input } from "$lib/components/ui/input";
     import { Search } from "@lucide/svelte";
     import { Button } from "$lib/components/ui/button";
-    import * as Popover from "$lib/components/ui/popover";
     import { match } from "$lib/result";
     import { get, getPossibleMovies } from "$lib/middleware";
     import { type GuessDomain } from "$lib/domain";
@@ -16,7 +15,7 @@
     let guesses = $state([] as GuessDomain[]);
 
     let possibleGuesses = $state([] as string[]);
-    let filteredGuesses = $derived(find(guessValue, possibleGuesses));
+    let filteredGuesses = $derived(find(guessValue, possibleGuesses).filter((x, i) => i < 10));
     let searchOpen = $state(false);
 
     onMount(async () => {
@@ -68,39 +67,34 @@
 <div class="bg-red-500 p-4 flex justify-center min-h-screen">
     <div class="w-1/2 bg-blue-500 p-4">
         <div class="flex">
-            <Popover.Root bind:open={searchOpen}>
-                <Popover.Trigger asChild>
-                    <Input
-                        type="text"
-                        placeholder="Guess..."
-                        bind:value={guessValue}
-                        onchange={guessChange}
-                        class="m-1"
-                    />
+            <Input
+                type="text"
+                placeholder="Guess..."
+                bind:value={guessValue}
+                onchange={guessChange}
+                class="m-1"
+            />
 
-                    <Button
-                        type="submit"
-                        size="icon"
-                        onclick={handleGuess}
-                        class="m-1"
-                    >
-                        <Search />
-                    </Button>
-                </Popover.Trigger>
-                <Popover.Content class="w-lg">
-                    {#each filteredGuesses as possibleGuess}
-                        <p>{possibleGuess}</p>
-                    {/each}
-                </Popover.Content>
-            </Popover.Root>
+            <Button type="submit" size="icon" onclick={handleGuess} class="m-1">
+                <Search />
+            </Button>
         </div>
+
+        {#if filteredGuesses.length > 0}
+            <div class="mt-1 bg-white border border-gray-300 rounded shadow-lg z-10">
+                {#each filteredGuesses as possibleGuess}
+                    <p>{possibleGuess}</p>
+                {/each}
+            </div>
+        {/if}
+
         {#if errorMessage !== ""}
             <div class="text-red">
                 {errorMessage}
             </div>
         {/if}
 
-        <div class="guesses">
+        <div class="guesses z-10">
             {#each guesses as guess}
                 <Guess props={guess} />
             {/each}
