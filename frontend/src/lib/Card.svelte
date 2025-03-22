77338bc9d87fd936357f38d4a86b3cedb076f46f
Will Hodges <hodges.wt@gmail.com>
2025-03-22 18:05:21 +0000

    remove bullet points

--- frontend/src/lib/Card.svelte
+++ frontend/src/lib/Card.svelte
@@ -13,7 +13,7 @@
 <div class="p-2 h-full">
     <div class={`p-4 flex flex-col h-full rounded-lg shadow-md bg-${props.color}-300`}>
         <h2 class="text-lg font-bold mb-2">{props.title}</h2>
-        <ul class="list-disc list-inside flex-grow">
+        <ul class="list-none list-inside flex-grow">
             {#each props.data as datum}
                 <li class="text-black">{datum}</li>
             {/each}
