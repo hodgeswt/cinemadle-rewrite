ef1a86a85fb0c163330160d237586796efd9cb1f
Will Hodges <hodges.wt@gmail.com>
2025-03-22 15:21:30 +0000

    db migrations

--- backend/migrations/cli.rb
+++ backend/migrations/cli.rb
@@ -10,14 +10,20 @@ subcommand = ARGV[0]
 if subcommand == 'help'
   puts 'cinemadle migration cli'
   puts ''
-  puts "Usage: 'ruby cli.rb (help | new [\"description\"] | apply [all])'"
+  puts "Usage: 'ruby cli.rb (help | creds [db_name user password host port] | new [\"description\"] | apply [all])'"
   puts ''
   puts 'Example:'
   puts ''
   puts 'ruby cli.rb help'
-  puts '\t Prints this message'
+  puts "\tPrints this message"
   puts 'ruby cli.rb new "add user table"'
-  puts '\t Creates a new migration script called "add user table"'
+  puts "\tCreates a new migration script called \"add user table\""
+  puts 'ruby cli.rb creds database admin Password1 127.0.0.1 5432'
+  puts "\tSets credentials for cli to PG database \"database\" with user admin/Password1 at localhost:5432"
+  puts 'ruby cli.rb apply file_name'
+  puts "\tApplies migration \"file_name\""
+  puts 'ruby cli.rb apply all'
+  puts "\tApplies all migrations"
   exit 0
 end
 
@@ -28,12 +34,51 @@ if subcommand == 'new'
   end
 
   puts 'Creating new migration...'
-  success = Migrate.generate_migration ARGV[1]
+  success = Migrate.generate_migration? ARGV[1]
 
   unless success
     puts 'Failed to create migration'
     exit 1
   end
 
+  exit 0
+end
+
+if subcommand == 'creds'
+  if ARGV.length != 6
+    puts 'Expected db_name, user, password, host, and port'
+    exit 1
+  end
+
+  puts "Creating and saving credentials to #{Migrate::CREDS_FILE}"
+
+  unless Migrate.set_credentials?(ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5])
+    puts 'Failed to set credentials'
+    exit 1
+  end
+
+  puts 'Credentials created'
+  exit 0
+end
+
+if subcommand == 'apply'
+  if ARGV.length != 2
+    puts 'Expected migration name or "all"'
+    exit 1
+  end
+
+  if ARGV[1] == 'all'
+    unless Migrate.apply_all?
+      puts 'Failed to apply all migrations'
+      exit 1
+    end
+  else
+    unless Migrate.apply_migration?(ARGV[1])
+      puts 'Failed to apply migration'
+      exit 1
+    end
+  end
+
+  puts 'Migration applied'
   exit 0
 end
--- backend/migrations/migrate.rb
+++ backend/migrations/migrate.rb
@@ -1,22 +1,87 @@
+#!/usr/bin/env ruby
+
 require 'date'
+require 'pg'
+require 'yaml'
 
 module Migrate
-  def self.validate_description(description)
-    unless description.instance_of? String
-      puts 'Expected string input for description.'
+  CREDS_FILE = './credentials.yaml'.freeze
+
+  class Credentials
+    attr_accessor :db_name, :user, :password, :host, :port
+
+    def self.validate_input?(db_name, user, password, host, port)
+      a = Migrate.validate_string? db_name
+      b = Migrate.validate_string? user
+      c = Migrate.validate_string? password
+      d = Migrate.validate_string? host
+      e = Migrate.validate_string? port
+
+      a && b && c && d && e
+    end
+
+    def initialize(db_name, user, password, host, port)
+      unless Credentials.validate_input?(db_name, user, password, host, port)
+        raise 'Expected db_name, user, password, host, and port to be non-empty'
+      end
+
+      @db_name = db_name
+      @user = user
+      @password = password
+      @host = host
+      @port = port
+    end
+
+    def to_s
+      "db_name: #{db_name}, user: #{user}, password: ***, host: #{host}, port: #{port}"
+    end
+  end
+
+  def self.set_credentials?(db_name, user, password, host, port)
+    return false unless Credentials.validate_input?(db_name, user, password, host, port)
+
+    c = Credentials.new(db_name, user, password, host, port)
+
+    File.write(CREDS_FILE, c.to_yaml)
+    true
+  end
+
+  def self.with_db(c)
+    raise 'Invalid credentials' unless c.instance_of? Credentials
+
+    db = PG.connect(
+      dbname: c.db_name,
+      user: c.user,
+      password: c.password,
+      host: c.host,
+      port: c.port
+    )
+
+    begin
+      yield db
+    ensure
+      db.close
+    end
+  end
+
+  def self.validate_string?(value)
+    unless value.instance_of? String
+      puts 'Expected string input.'
       return false
     end
 
-    re = /^\w*$/
+    re = /^\s*$/
 
-    return true unless re.match?(description)
+    if re.match?(value)
+      puts "Expected value #{value} to be non-empty"
+      return false
+    end
 
-    puts 'Expected description to be non-empty'
-    false
+    true
   end
 
-  def self.generate_migration(description)
-    return false unless validate_description(description)
+  def self.generate_migration?(description)
+    return false unless validate_string?(description)
 
     d = DateTime.now.to_s.gsub('-', '').gsub(':', '').gsub('+', '')
 
@@ -24,10 +89,42 @@ module Migrate
     file_path = "sql/#{d}_#{file_description}.sql"
 
     File.open(file_path, 'w') do |f|
-      f.write("-- DB migration #{description} generated on #{DateTime.now}")
+      f.write("-- DB migration #{description} generated on #{DateTime.now}\nINSERT INTO migration_history (migration) VALUES ('#{d}_#{file_description}')")
     end
 
     puts "Generated migration #{file_path}"
+    true
+  end
+
+  def self.apply_all?
+    Dir.glob('./sql/*.sql') do |migration|
+      return false unless apply_migration? migration
+    end
+
+    true
+  end
+
+  def self.apply_migration?(name)
+    return false unless validate_string?(name)
+
+    name.gsub!(%r{^(\./)?sql/}, '')
+    name = "./sql/#{name}"
+    unless File.exist? name
+      puts "Migration #{name} does not exist"
+      return false
+    end
+
+    script = File.open(name, 'rb', &:read)
+    credentials = YAML.load_file(CREDS_FILE, permitted_classes: [Migrate::Credentials])
+    puts "Connecting with #{credentials}"
+    with_db(credentials) do |db|
+      db.exec(script)
+    rescue PG::Error
+      puts 'DB error'
+      exit 1
+    end
+
+    puts 'Migration successful'
     true
   end
 end
