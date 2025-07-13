63ae407e68360615a3915aa68a4d2b2aa08e08ae
Will Hodges <hodges.wt@gmail.com>
2025-07-13 01:58:47 +0000

    persistent user store

--- frontend/src/lib/stores/user.svelte.ts
+++ frontend/src/lib/stores/user.svelte.ts
@@ -1,10 +1,53 @@
+import { hasValue } from '$lib/util';
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+
+type UserStoreData = {
+    email: string;
+    jwt: string;
+    loggedIn: boolean;
+}
+
+function isUserStoreData(obj: any): obj is UserStoreData {
+    if (!hasValue(obj)) {
+        return false;
+    }
+
+    return typeof obj.email === 'string'
+        && typeof obj.jwt === 'string'
+        && typeof obj.loggedIn === 'boolean'
+}
 
 const createUserStore = () => {
+    const localStorageKey = "cinemadleUserStore";
+    const rawCached = browser && localStorage.getItem(localStorageKey) || null;
+    const getCached = (): UserStoreData | null => {
+        try {
+            const j = JSON.parse(rawCached === null ? "" : rawCached);
+            if (!isUserStoreData(j)) {
+                return null;
+            }
+
+            return j as UserStoreData
+        } catch {
+            return null
+        }
+    }
+
+    const userStoreCached = getCached();
+
+    const defaultEmail = userStoreCached === null ? '' : userStoreCached.email;
+    const defaultJwt = userStoreCached === null ? '' : userStoreCached.jwt;
+    const defaultLoggedIn = userStoreCached === null ? '' : userStoreCached.loggedIn;
+
     const { subscribe, set, update } = writable({
-        email: '',
-        jwt: '',
-        loggedIn: false,
+        email: defaultEmail,
+        jwt: defaultJwt,
+        loggedIn: defaultLoggedIn,
+    });
+
+    subscribe(user => {
+        browser && localStorage.setItem("cinemadleUserStore", JSON.stringify(user));
     });
 
     return {
