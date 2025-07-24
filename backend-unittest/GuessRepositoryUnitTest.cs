3a53c11de8c8a46caf056680a3f741fc4b0910fd
Will Hodges <hodges.wt@gmail.com>
2025-07-24 23:41:34 +0000

    add more unit tests

--- backend-unittest/GuessRepositoryUnitTest.cs
+++ backend-unittest/GuessRepositoryUnitTest.cs
@@ -114,4 +114,153 @@ public class GuessRepositoryUnitTest
         Assert.Equivalent(expected, o);
     }
 
+    [Fact]
+    public void CastGreyNoBoldTest()
+    {
+        GuessRepository guessRepo = GetGuessRepository();
+        MovieDto guess = GetTargetMovie();
+        guess.Cast = [
+            new PersonDto { Name = "Harrison Ford", Role = "Cast" },
+            new PersonDto { Name = "Tom Hiddleston", Role = "Cast" },
+            new PersonDto { Name = "Emilia Clarke", Role = "Cast" }
+        ];
+        MovieDto target = GetTargetMovie();
+
+        GuessDto expected = new GuessDto
+        {
+            Fields = new Dictionary<string, FieldDto>()
+            {
+                { "boxOffice", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = [ target.BoxOffice.ToString() ],
+                        Modifiers = new(),
+                    }
+                },
+                { "creatives", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = target.Creatives.Select(x => GuessRepository.CreativeFromPerson(x)),
+                        Modifiers = new(),
+                    }
+                },
+                { "cast", new FieldDto
+                    {
+                        Color = "grey",
+                        Direction = 0,
+                        Values = guess.Cast.Select(x => x.Name),
+                        Modifiers = new(),
+                    }
+                },
+                { "rating", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = [ target.Rating.ToString() ],
+                        Modifiers = new(),
+                    }
+                },
+                { "genre", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = target.Genres,
+                        Modifiers = new(),
+                    }
+                },
+                { "year", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = [ target.Year ],
+                        Modifiers = new(),
+                    }
+                }
+
+            }
+        };
+
+        GuessDto o = guessRepo.Guess(guess, target);
+
+        Assert.Equivalent(expected, o);
+    }
+
+    [Fact]
+    public void CastYellowTest()
+    {
+        GuessRepository guessRepo = GetGuessRepository();
+        MovieDto guess = GetTargetMovie();
+        guess.Cast = [
+            new PersonDto { Name = "Mike Myers", Role = "Cast" },
+            new PersonDto { Name = "Tom Hiddleston", Role = "Cast" },
+            new PersonDto { Name = "Emilia Clarke", Role = "Cast" }
+        ];
+        MovieDto target = GetTargetMovie();
+
+        GuessDto expected = new GuessDto
+        {
+            Fields = new Dictionary<string, FieldDto>()
+            {
+                { "boxOffice", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = [ target.BoxOffice.ToString() ],
+                        Modifiers = new(),
+                    }
+                },
+                { "creatives", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = target.Creatives.Select(x => GuessRepository.CreativeFromPerson(x)),
+                        Modifiers = new(),
+                    }
+                },
+                { "cast", new FieldDto
+                    {
+                        Color = "yellow",
+                        Direction = 0,
+                        Values = guess.Cast.Select(x => x.Name),
+                        Modifiers = new Dictionary<string, List<string>>()
+                        {
+                            { "Mike Myers", new List<string>() { "bold" } },
+                        }
+                    }
+                },
+                { "rating", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = [ target.Rating.ToString() ],
+                        Modifiers = new(),
+                    }
+                },
+                { "genre", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = target.Genres,
+                        Modifiers = new(),
+                    }
+                },
+                { "year", new FieldDto
+                    {
+                        Color = "green",
+                        Direction = 0,
+                        Values = [ target.Year ],
+                        Modifiers = new(),
+                    }
+                }
+
+            }
+        };
+
+        GuessDto o = guessRepo.Guess(guess, target);
+
+        Assert.Equivalent(expected, o);
+    }
+
 }
