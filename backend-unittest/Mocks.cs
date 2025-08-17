688d9fcc877f2c1f0ae9e876cfdea1025ebf6cd5
Will Hodges <hodges.wt@gmail.com>
2025-08-17 17:26:56 -0400

    update unit tests

--- backend-unittest/Mocks.cs
+++ backend-unittest/Mocks.cs
@@ -16,6 +16,8 @@ public class Mocks
     private static readonly CinemadleConfig _defaultConfig = new()
     {
         TmdbApiKey = string.Empty,
+        PaymentsApiKey = "asdf",
+        WebhookSecret = "asdf",
         CastCount = 3,
         GenresCount = 3,
         CacheTTL = 10,
@@ -36,6 +38,9 @@ public class Mocks
             { "9", 6.0F }
         },
         GameLength = 10,
+        PaymentSuccessUrl = "http://localhost:5173/paymentSuccess",
+        PaymentFailureUrl = "http://localhost:5173/paymentFailure",
+        AddOnMapping = []
     };
 
     public static IMemoryCache GetMemoryCache()
