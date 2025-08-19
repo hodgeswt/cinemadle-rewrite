689a87da1c26f3f58c5b171dc8349925bf4dc6e3
Will Hodges <hodges.wt@gmail.com>
2025-08-19 17:58:15 -0400

    fix unit tests

--- backend-dotnet/Database/IdentityContext.cs
+++ backend-dotnet/Database/IdentityContext.cs
@@ -4,15 +4,24 @@ using Microsoft.EntityFrameworkCore;
 
 public class IdentityContext : IdentityDbContext<IdentityUser>
 {
-    public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+    public string DbPath { get; set; }
+
+    public IdentityContext(DbContextOptions<IdentityContext> opts) : base(opts)
     {
         var folder = Environment.SpecialFolder.LocalApplicationData;
         string path = Environment.GetFolderPath(folder);
-        DbPath = Path.Join(path, "AppData", "cinemadle-identity.db");
+        DbPath = Path.Join(path, "AppData", "cinemadle.db");
     }
 
-    public string DbPath { get; set; }
+    public IdentityContext() : this(new DbContextOptions<IdentityContext>())
+    {
+    }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-        optionsBuilder.UseSqlite($"DataSource={DbPath}");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite($"DataSource={DbPath}");
+        }
+    }
 }
