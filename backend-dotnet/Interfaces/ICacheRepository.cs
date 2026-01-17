b0de5f73f3393bce6f5bf044b1ffca69e86be24f
Will Hodges <hodges.wt@gmail.com>
2026-01-17 18:15:29 -0500

    dont cache stuff

--- backend-dotnet/Interfaces/ICacheRepository.cs
+++ backend-dotnet/Interfaces/ICacheRepository.cs
@@ -4,4 +4,5 @@ public interface ICacheRepository
 {
     public bool Set(string key, object value);
     public bool TryGet<T>(string key, out T? value) where T : class;
+    public void Remove(string key);
 }
